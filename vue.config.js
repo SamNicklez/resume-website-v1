8e451645510b3f92bd1e719b6581a140e3946ffb
SamNicklez <samuelnicklaus1@gmail.com>
2023-01-08 14:27:40 -0600

    commit

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/vue-pages/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue-pages/'
+    : '/',
   transpileDependencies: true
 })
