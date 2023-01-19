e64ac44967f3495d30da69bd29bcf48be9c06d8d
SamNicklez <samuelnicklaus1@gmail.com>
2023-01-19 11:31:48 -0600

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
+<script>
+
+</script>
+
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="mainContainer">
+    <div class="leftCardContainer">
+      <b-card no-body style="max-width: 20rem;" img-src="https://i.imgur.com/6pyE2HN.jpg" img-alt="Image" img-top>
+        <b-card-body>
+          <b-card-title>Samuel Nicklaus</b-card-title>
+          <b-card-sub-title class="h5 mb-2">Info</b-card-sub-title>
+          <b-list-group flush>
+            <b-list-group-item>
+              <p class="h4 mb-2"><b-icon icon="envelope-fill"></b-icon> Email</p>
+              <b-card-sub-title class="mb-2">SNicklaus@uiowa.edu</b-card-sub-title>
+            </b-list-group-item>
+            <b-list-group-item>
+              <p class="h4 mb-2"><b-icon icon="telephone-fill"></b-icon> Phone</p>
+              <b-card-sub-title class="mb-2">(319)-333-5747</b-card-sub-title>
+            </b-list-group-item>
+            <b-list-group-item>
+              <p class="h4 mb-2"><b-icon icon="person-fill"></b-icon> Birthday</p>
+              <b-card-sub-title class="mb-2">Feb 6, 2002</b-card-sub-title>
+            </b-list-group-item>
+            <b-list-group-item>
+              <p class="h4 mb-2"><b-icon icon="pin-map-fill"></b-icon> Residence</p>
+              <b-card-sub-title class="mb-2">Iowa City, Iowa</b-card-sub-title>
+            </b-list-group-item>
+          </b-list-group>
+          <b-card-sub-title class="h4 mb-2">Socials</b-card-sub-title>
+          <b-list-group flush>
+            <b-list-group-item>
+              <p class="h4 mb-2"><b-icon icon="github"></b-icon> GitHub</p>
+              <b-card-sub-title class="mb-2"><a href="https://github.com/SamNicklez">github.com/SamNicklez</a></b-card-sub-title>
+            </b-list-group-item>
+            <b-list-group-item>
+              <p class="h4 mb-2"><b-icon icon="linkedin"></b-icon> LinkedIn</p>
+              <b-card-sub-title class="mb-2"><a href="https://www.linkedin.com/in/sam-nicklaus/">linkedin.com/sam-nicklaus</a></b-card-sub-title>
+            </b-list-group-item>
+            <b-list-group-item>
+              <p class="h4 mb-2"><b-icon icon="person-fill"></b-icon> Birthday</p>
+              <b-card-sub-title class="mb-2">Feb 6, 2002</b-card-sub-title>
+            </b-list-group-item>
+            <b-list-group-item>
+              <p class="h4 mb-2"><b-icon icon="pin-map-fill"></b-icon> Residence</p>
+              <b-card-sub-title class="mb-2">Iowa City, Iowa</b-card-sub-title>
+            </b-list-group-item>
+          </b-list-group>
+        </b-card-body>
+      </b-card>
+    </div>
+    <div class="rightCardContainer">
+
+    </div>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.leftCardContainer {
+  margin-left: 20%;
+  margin-top: 1%;
+}
+.mainContainer{
+  background-color: rgb(198, 241, 200);
+  width: 100%;
+  height: 100%;
 }
 </style>
