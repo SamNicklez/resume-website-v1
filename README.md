3813608e4ec72c7992f634d2fcd0cc9fad369c41
snicklaus <snicklaus@uiowa.edu>
2023-02-21 15:13:16 -0600

    commit

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # vue-pages
 
+## Pre-Project setup
+```
+Install Node.js (Note: Version has to be 16 or lower to function)
+
+```
+
 ## Project setup
 ```
+CD to source folder of the repository and run
+
 npm install
 ```
 
@@ -18,6 +26,19 @@ npm run build
 ### Lints and fixes files
 ```
 npm run lint
+```
+
+### To push to github for public viewing
+```
+Note: Beforehand you need to edit the deploy.sh file to match your new GitHub repo
+npm run deploy
+```
+
+### Changing assets
+```
+To change the icon logo, change favicon.ico in the public folder
+To change the resume download, change snicklausResume.pdf to your desired resume (Note that you also will need to change the download path in App.vue for the changes to take effect)
+
 ```
 
 ### Customize configuration
--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,30 @@ export default {
   data() {
     return {
       //Values below can be changed as needed easily in case something changes
-      name: "Samuel Nicklaus",
-      email: "snicklaus@uiowa.edu",
-      phone: "(319)-333-5747",
-      bday: "Feb 6, 2002",
-      location: "Iowa City, Iowa",
-      gitDisplay: "github.com/SamNicklez",
-      gitURL: "https://github.com/SamNicklez",
-      linkedInDisplay: "linkedin.com/sam-nicklaus",
-      linkedInURL: "https://www.linkedin.com/in/sam-nicklaus/",
-      photoSrc: "https://i.imgur.com/hCDgktV.jpg",
-      publicPath: process.env.BASE_URL,
-      biotext:`As a Software Engineering Intern at Collins Aerospace, I am dedicated to developing innovative products and
-            services that streamline manufacturing processes and increase efficiency. With a strong background in
-            engineering and a passion for technology, I am excited to take on new challenges and continue learning in
-            the dynamic aerospace and defense industry. In the near future, I will have the opportunity to further my
-            skills and knowledge as a DLDP Intern at Raytheon Technologies, a leading global provider of advanced
-            defense and aerospace technologies.`,
+      name: "Samuel Nicklaus", //Name displayed on the left card
+      email: "snicklaus@uiowa.edu", //Email displayed on the left card
+      phone: "(319)-333-5747", //Phone displayed on the left card
+      bday: "Feb 6, 2002", //DOB displayed on the left card
+      location: "Iowa City, Iowa", //Location displayed on the left card
+      gitDisplay: "github.com/SamNicklez", //URL that is displayed to the user
+      gitURL: "https://github.com/SamNicklez", //URL that the user goes to on click
+      linkedInDisplay: "linkedin.com/sam-nicklaus", //URL that is displayed to the user to be clicked
+      linkedInURL: "https://www.linkedin.com/in/sam-nicklaus/", //URL user goes to on link click
+      photoSrc: "https://i.imgur.com/hCDgktV.jpg", //Source for headshot photo at top of screen
+      publicPath: process.env.BASE_URL, //Base url, used to grab resume.pdf
+      //Text that is displayed at the top of the right card
+      biotext: [
+        {
+          text: `As a student at the University of Iowa pursuing a degree in Computer Science and Engineering, I have gained a solid foundation in software development, data structures, algorithms, and computer architecture. Prior to my current studies, I completed an internship as a software engineer at Collins Aerospace, where I was involved in developing and maintaining software applications for critical manufacturing processes.`
+        },
+        {
+          text: `During my internship, I had the opportunity to work with a team of engineers and collaborate with other departments to ensure the successful delivery of projects. Through this experience, I developed skills in problem-solving, critical thinking, and effective communication. I am confident that these skills will be valuable in any future roles I take on.`
+        },
+        {
+          text: `With my passion for technology, I am excited to contribute my skills and knowledge to any team I join. I am eager to continue learning and growing in my career and am open to new challenges and opportunities.`
+        }
+      ],
+      //Images and text that can be shown to show off your hobbies
       images: [
         {
           link: 'https://i.imgur.com/Y1gDzjX.jpg',
@@ -29,9 +36,17 @@ export default {
           link: 'https://i.imgur.com/0yNW4hc.jpg',
           description: 'eSports Coaching'
         }
-              ],
+      ],
+      //Formats timeline
+      //Title: Displayed at the top of the event
+      //description: description of the event
+      //Date: date the event happened
+      //Category: text displayed in the pill at the top of the event
+      //photo: provided a link, the photo will be displayed alongside the event
+      //color: changes the color theme of the event, available colors include
+      //| "black" | "blue" | "green" | "orange" | "purple" | "red" | "turquoise" | "white"
       events: [
-      {
+        {
           title: 'Note',
           date: new Date('02/21/2023'),
           description: `Timeline is still in progress...`,
@@ -122,7 +137,9 @@ export default {
         },
 
       ],
-
+      //Used on displaying skills with the progress bar where
+      //Name: what is displayed alongside the progress bar
+      //progress: the percent you want the bar filled
       languages: [
         {
           name: 'C#',
@@ -186,95 +203,117 @@ export default {
         }
       ],
     }
-  }
+  },
+  methods: {
+    isMobile() {
+      if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        return true
+      } else {
+        return false
+      }
+    }
+  },
 }
 </script>
 
 <template>
-  <div class="mainContainer">
-    <div class="topContainer">
-      <div class="leftCardContainer">
-        <b-card class="text-center">
-          <b-avatar :src="photoSrc" size="15rem"></b-avatar>
-          <b-card-body>
-            <b-card-title>{{ name }}</b-card-title>
-            <b-card-sub-title class="h5 mb-2">Info</b-card-sub-title>
-            <b-list-group flush>
-              <b-list-group-item>
-                <p class="h4 mb-2"><b-icon icon="envelope-fill"></b-icon> Email</p>
-                <b-card-sub-title class="mb-2">{{ email }}</b-card-sub-title>
-              </b-list-group-item>
-              <b-list-group-item>
-                <p class="h4 mb-2"><b-icon icon="telephone-fill"></b-icon> Phone</p>
-                <b-card-sub-title class="mb-2">{{ phone }}</b-card-sub-title>
-              </b-list-group-item>
-              <b-list-group-item>
-                <p class="h4 mb-2"><b-icon icon="person-fill"></b-icon> Birthday</p>
-                <b-card-sub-title class="mb-2">{{ bday }}</b-card-sub-title>
-              </b-list-group-item>
-              <b-list-group-item>
-                <p class="h4 mb-2"><b-icon icon="pin-map-fill"></b-icon> Residence</p>
-                <b-card-sub-title class="mb-2">{{ location }}</b-card-sub-title>
-              </b-list-group-item>
-            </b-list-group>
-            <b-card-sub-title class="h4 mb-2"> Socials</b-card-sub-title>
-            <b-list-group flush>
-              <b-list-group-item>
-                <p class="h4 mb-2"><b-icon icon="github"></b-icon> GitHub</p>
-                <b-card-sub-title class="mb-2"><a :href="gitURL"> {{ gitDisplay }} </a></b-card-sub-title>
-              </b-list-group-item>
-              <b-list-group-item>
-                <p class="h4 mb-2"><b-icon icon="linkedin"></b-icon> LinkedIn</p>
-                <b-card-sub-title class="mb-2"><a :href="linkedInURL">{{
-                  linkedInDisplay
-                }}</a></b-card-sub-title>
-              </b-list-group-item>
-              <b-list-group-item>
-                <p class="h4 mb-2"><b-icon icon="folder-fill"></b-icon> Full Resume</p>
-                <b-card-sub-title class="mb-2"><a :href="linkedInURL">
-                  <a :href="`${publicPath}snicklausResume.pdf`" download="snicklausResume.pdf">Click to Download</a>
-                </a></b-card-sub-title>
-              </b-list-group-item>
-            </b-list-group>
-          </b-card-body>
-        </b-card>
-      </div>
-      <div class="rightCardContainer">
-        <b-card title="Bio" sub-title="Who Am I?">
-          <b-card-text>
-            {{ this.biotext }}
-          </b-card-text>
-          <b-card title="Education" sub-title="August 2020 - May 2024" img-src="https://i.imgur.com/y7MGuUn.png"
-            img-height="150" img-width="150" class="cardFormat" img-alt="Card image" img-right>
-            <b-card-text>
-              <b>University of Iowa</b>
-              <br>
-              <i>Bachelor of Science in Engineering, Computer Science & Engineering</i>
-              <br>
-              <i>Focus Area in Big Data & Machine Learning</i>
-              <br>
-              <i>Minor in Mathematics</i>
-            </b-card-text>
+  <div v-if="!isMobile()">
+    <desktop>
+      <div class="mainContainer">
+        <div class="topContainer">
+          <div class="leftCardContainer">
+            <b-card style=" height: 100%;">
+              <b-avatar :src="photoSrc" size="15rem"></b-avatar>
+              <b-card-body>
+                <b-card-title>{{ name }}</b-card-title>
+                <b-card-sub-title class="h5 mb-2">Info</b-card-sub-title>
+                <b-list-group flush>
+                  <b-list-group-item>
+                    <p class="h4 mb-2"><b-icon icon="envelope-fill"></b-icon> Email</p>
+                    <b-card-sub-title class="mb-2">{{ email }}</b-card-sub-title>
+                  </b-list-group-item>
+                  <b-list-group-item>
+                    <p class="h4 mb-2"><b-icon icon="telephone-fill"></b-icon> Phone</p>
+                    <b-card-sub-title class="mb-2">{{ phone }}</b-card-sub-title>
+                  </b-list-group-item>
+                  <b-list-group-item>
+                    <p class="h4 mb-2"><b-icon icon="person-fill"></b-icon> Birthday</p>
+                    <b-card-sub-title class="mb-2">{{ bday }}</b-card-sub-title>
+                  </b-list-group-item>
+                  <b-list-group-item>
+                    <p class="h4 mb-2"><b-icon icon="pin-map-fill"></b-icon> Residence</p>
+                    <b-card-sub-title class="mb-2">{{ location }}</b-card-sub-title>
+                  </b-list-group-item>
+                </b-list-group>
+                <b-card-sub-title class="h4 mb-2"> Socials</b-card-sub-title>
+                <b-list-group flush>
+                  <b-list-group-item>
+                    <p class="h4 mb-2"><b-icon icon="github"></b-icon> GitHub</p>
+                    <b-card-sub-title class="mb-2"><a :href="gitURL" target="_blank"> {{ gitDisplay }}
+                      </a></b-card-sub-title>
+                  </b-list-group-item>
+                  <b-list-group-item>
+                    <p class="h4 mb-2"><b-icon icon="linkedin"></b-icon> LinkedIn</p>
+                    <b-card-sub-title class="mb-2"><a :href="linkedInURL" target="_blank">{{
+                      linkedInDisplay
+                    }}</a></b-card-sub-title>
+                  </b-list-group-item>
+                  <b-list-group-item>
+                    <p class="h4 mb-2"><b-icon icon="folder-fill"></b-icon> Full Resume</p>
+                    <b-card-sub-title class="mb-2"><a :href="linkedInURL">
+                        <a :href="`${publicPath}snicklausResume.pdf`" download="snicklausResume.pdf">Click to Download</a>
+                      </a></b-card-sub-title>
+                  </b-list-group-item>
+                </b-list-group>
+              </b-card-body>
+            </b-card>
+          </div>
+          <div class="rightCardContainer">
+            <b-card title="Bio" sub-title="Who Am I?" style=" height: 100%;">
+              <b-card-text>
+                <div v-for="text in biotext" :key="text.id">
+                  {{ text.text }}<br><br>
+                </div>
+              </b-card-text>
+              <b-card title="Education" sub-title="August 2020 - May 2024" img-src="https://i.imgur.com/y7MGuUn.png"
+                img-height="150" img-width="150" class="cardFormat" img-alt="Card image" img-right>
+                <b-card-text>
+                  <b>University of Iowa</b>
+                  <br>
+                  <i>Bachelor of Science in Engineering, Computer Science & Engineering</i>
+                  <br>
+                  <i>Focus Area in Big Data & Machine Learning</i>
+                  <br>
+                  <i>Minor in Mathematics</i>
+                </b-card-text>
+              </b-card>
+              <b-card title="Skills" sub-title="Languages and Frameworks (% are not absolute)" class="cardFormat"
+                horizontal>
+                <b-card-group deck v-for="name in languages" :key="name">
+                  <b-list-group-item class="languages">
+                    {{ name.name }}
+                    <b-progress :value="name.progress" class="w-50" variant="secondary" height="5px"></b-progress>
+                  </b-list-group-item>
+                </b-card-group>
+              </b-card>
+            </b-card>
+          </div>
+        </div>
+        <div class="bottomCard">
+          <b-card title="Timeline" class="cardFormat">
+            <div v-for="title in events" :key="title.id">
+              <vue-timeline-update theme="light" :date="title.date" :title="title.title" :description="title.description"
+                :thumbnail="title.photo" :category="title.category" :icon="title.icon" :color="title.color" />
+          </div>
           </b-card>
-          <b-card title="Skills" sub-title="Languages and Frameworks (% are not absolute)" class="cardFormat" horizontal>
-            <b-card-group deck v-for="name in languages" :key="name">
-              <b-list-group-item class="languages">
-                {{ name.name }}
-                <b-progress :value="name.progress" class="w-50" variant="secondary" height="5px"></b-progress>
-              </b-list-group-item>
-            </b-card-group>
-          </b-card>
-        </b-card>
+        </div>
       </div>
-    </div>
-    <div class="bottomCard">
-      <b-card title="Timeline" class="cardFormat">
-        <li v-for="title in events" :key="title.id">
-          <vue-timeline-update theme="light" :date="title.date" :title="title.title" :description="title.description"
-            :thumbnail="title.photo" :category="title.category" :icon="title.icon" :color="title.color" />
-        </li>
-      </b-card>
-    </div>
+    </desktop>
+  </div>
+  <div v-else>
+    <mobile>
+      Mobile device detected, please run in desktop mode while I am working on the mobile site :)
+    </mobile>
   </div>
 </template>
 
@@ -302,10 +341,6 @@ export default {
   vertical-align: baseline;
 }
 
-.ProgressText {
-  min-width: 75vh;
-}
-
 .progressContainer {
   margin-top: 1vh;
   display: flex;
@@ -316,18 +351,18 @@ export default {
 }
 
 .leftCardContainer {
-  float: left;
   margin-top: 2vh;
   min-width: 24.5%;
   margin-left: 10%;
+  text-align: center;
+  margin-right: 1%;
 }
 
 .rightCardContainer {
+  float: right;
   margin-top: 2vh;
   margin-right: 10%;
   min-width: 49.5%;
-  margin-left: 1%;
-  float: left;
 }
 
 .mainContainer {
